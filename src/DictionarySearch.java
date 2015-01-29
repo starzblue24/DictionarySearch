9c622f4b1bc06c4ed2a82741cfaaf03565e7ad25
Naimisha Badri <starzblue24@gmail.com>
2015-01-29 07:30:33 -0700

    Update DictionarySearch.java

--- src/DictionarySearch.java
+++ src/DictionarySearch.java
@@ -2,78 +2,81 @@ import java.io.*;
 import java.util.*;
 
 public class DictionarySearch {
-
-	/**
-	 * stores an ordered lists of words for searching
-	 */
-	private static ArrayList<String> words;
-
-	/**
-	 * main program
-	 * @param args
-	 * @throws IOException
-	 */
-	public static void main(String[] args) throws IOException {
-
-		words = getWords();
-		int interval = 100;
-		String[] find = new String[100];    
-		int index = interval;
-		for (int i = 0; i < interval - 1; i++) {
-			index += words.size() / interval;
-			find[i] = words.get(index);
-		}
-		find[interval - 1] = "Xdfsdda";
-
-		SearchResult seqResult;
-		SearchResult binResult;
-
-		System.out.println("#\tBinary\tSequential\tindex\tword");
-
-		for (int i = 0; i < find.length; i++) {
-			seqResult = sequentialSearch(find[i]);
-			binResult = binarySearch(find[i]);
-			System.out.println(i + "\t" + binResult.getIterations() + "\t" + seqResult.getIterations() + "\t" + binResult.getIndex() + ":" + seqResult.getIndex() + "\t" + find[i] );
-		}
-	}
-
-	/**
-	 * Implement a sequential search to find wordToFind in the ArrayList words
-	 * 
-	 * @param wordToFind - String to find in words
-	 * @return a SearchResult (index of item found or -1 if not found, number of iterations in search loop)
-	 */
-	public static SearchResult sequentialSearch(String wordToFind) {
-       int iterations=1;
-       SearchResult j;
-       for (int I = 0; I < words.size(); I++) {
-         iterations++;
-         if (words.get(I).compareTo(wordToFind)==0) {
-         return j=new SearchResult(I, iterations);;
+  
+  /**
+   * stores an ordered lists of words for searching
+   */
+  private static ArrayList<String> words;
+  
+  /**
+   * main program
+   * @param args
+   * @throws IOException
+   */
+  public static void main(String[] args) throws IOException {
+    
+    words = getWords();
+    int interval = 100;
+    String[] find = new String[100];    
+    int index = interval;
+    for (int i = 0; i < interval - 1; i++) {
+      index += words.size() / interval;
+      find[i] = words.get(index);
+    }
+    find[interval - 1] = "Xdfsdda";
+    
+    SearchResult seqResult;
+    SearchResult binResult;
+    
+    System.out.println("#\tBinary\tSequential\tindex\tword");
+    
+    for (int i = 0; i < find.length; i++) {
+      seqResult = sequentialSearch(find[i]);
+      binResult = binarySearch(find[i]);
+      System.out.println(i + "\t" + binResult.getIterations() + "\t" + seqResult.getIterations() + "\t" + binResult.getIndex() + ":" + seqResult.getIndex() + "\t" + find[i] );
+    }
+  }
+  
+  /**
+   * Implement a sequential search to find wordToFind in the ArrayList words
+   * 
+   * @param wordToFind - String to find in words
+   * @return a SearchResult (index of item found or -1 if not found, number of iterations in search loop)
+   */
+  public static SearchResult sequentialSearch(String wordToFind) {
+    int t=0;
+    
+    for (int I = 0; I < words.size(); I++) {
+      t++;
+      if (words.get(I).compareTo(wordToFind)==0) {
+        return new SearchResult(I, t);
       }
     }
-    return j=new SearchResult(0, iterations);;
-	}
-
-	/**
-	 * Implement a binary search to find wordToFind in the ArrayList words
-	 * 
-	 * @param wordToFind - String to find in words
-	 * @return a SearchResult (index of item found or -1 if not found, number of iterations in search loop)
-	 */
-	public static SearchResult binarySearch(String wordToFind) {
-          int min=0;
+    return new SearchResult(-1, t);
+  }
+  
+  
+  
+  /**
+   * Implement a binary search to find wordToFind in the ArrayList words
+   * 
+   * @param wordToFind - String to find in words
+   * @return a SearchResult (index of item found or -1 if not found, number of iterations in search loop)
+   */
+  public static SearchResult binarySearch(String wordToFind) {
+    int min=0;
     int max=words.size()-1;
     int mid;
-    SearchResult k;
+    int count=0;
     while(min<=max)
     {
+      count ++;
       mid=(max+min)/2;
-      if(words(mid)==wordToFind)
+      if(words.get(mid).compareTo(wordToFind)==0)
       {
-        return k=new SearchResult(1,0);
+        return new SearchResult(mid,count);
       }
-      else if(words(mid)<wordToFind)
+      else if(words.get(mid).compareTo(wordToFind)<0)
       {
         min=mid+1;
       }
@@ -82,23 +85,22 @@ public class DictionarySearch {
         max=mid-1;
       }
     }
-    return k=new SearchResult(-1,0);
-	}
-
-	/**
-	 * create an ArrayList<String> and populate it from text file
-	 * 
-	 * @return an ArrayList<String>
-	 * @throws FileNotFoundException
-	 */
-	public static ArrayList<String> getWords() throws FileNotFoundException {
-		ArrayList<String> result = new ArrayList<String>();
-		Scanner input = new Scanner(new File("words.txt"));
-		while(input.hasNextLine()) {
-			result.add(input.nextLine());
-		}
-		input.close();
-		return result;
-	}
+    return new SearchResult(-1,count);
+  }
+  /**
+   * create an ArrayList<String> and populate it from text file
+   * 
+   * @return an ArrayList<String>
+   * @throws FileNotFoundException
+   */
+  public static ArrayList<String> getWords() throws FileNotFoundException {
+    ArrayList<String> result = new ArrayList<String>();
+    Scanner input = new Scanner(new File("words.txt"));
+    while(input.hasNextLine()) {
+      result.add(input.nextLine());
+    }
+    input.close();
+    return result;
+  }
 }
 
